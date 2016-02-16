3c3ce72752d422c5fc5d608c8ee6f8f98baa8e2a
Haridu <haridu.naveen@gmail.com>
2016-02-16 15:18:49 -0600

    fixed argument string length

--- send-mail.py
+++ send-mail.py
@@ -13,25 +13,29 @@ def main():
                         required=True, help="Sender email address (Gmail)")
     parser.add_argument("-t", "--sendto",
                         required=True,
-                        help="name of the text file with a list of emails that \
-                              the email should send to")
+                        help="name of the text file with a list of emails that"
+                              " the email should send to")
     parser.add_argument("-b", "--body",
-                        default="This is a test to validate your email address. Please \
-                                  disregard this message.",
+                        default="This is a test to validate your email "
+                        "address. Please disregard this message.",
                         help="email body plain text")
     parser.add_argument("-s", "--smtp-server",
                         default='smtp.gmail.com',
+                        dest='server',
                         help="SMTP server")
     parser.add_argument("-p", "--smtp-port",
                         default=587,
+                        dest='port',
                         help="SMTP server port")
     args = parser.parse_args()
 
     sender = args.sendfrom
     mail_list = read_mail_list(args.sendto)
-    email = read_message(args.email)
+    body = args.body
+    smtp_server = args.server
+    smtp_port = args.port
 
-    send_email(sender, mail_list, email)
+    send_email(sender, mail_list, body, smtp_server, smtp_port)
 
 
 # reads a text file and returns a list of email addresses
@@ -55,22 +59,22 @@ def read_message(file):
 
 
 # sends the email
-def send_email(sender, receiver, email):
+def send_email(sender, mail_list, body, smtp_server, smtp_port):
     msg = MIMEMultipart()
     msg['From'] = sender
-    msg['To'] = ', '.join(receiver)
-    msg['Subject'] = email[0]
+    msg['To'] = ', '.join(mail_list)
+    msg['Subject'] = 'Test'
 
-    msg.attach(MIMEText(email[1], 'plain'))
+    msg.attach(MIMEText(body, 'plain'))
 
     pswd = getpass.getpass('Password: ')
 
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.login(sender, pswd)
         text = msg.as_string()
-        server.sendmail(sender, receiver, text)
+        server.sendmail(sender, mail_list, text)
         print "Email sent successfully"
     except smtplib.SMTPRecipientsRefused, e:
         print e.recipients
